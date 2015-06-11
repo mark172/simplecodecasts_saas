4a51f8ddedc436c12e53edf1b0df5bc5371a1dce
Mark Evangelista <mark172@me.com>
2015-06-11 03:16:05 +0000

    Added flash messages and form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,11 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
-      redirect_to new_contact_path, notice: "Message sent successfully!"
+      flash[:success] = "Message sent successfully!"
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: "Error, message not sent."
+      flash[:danger] = "An error occured. Message not sent."
+      redirect_to new_contact_path
     end
   end
   
